019a2de121012e5bcc5ed6d551a1dec4a4c68d32
lunor7649 <lunor7649@7HJL6Y1.Student.UGDSB.ED>
2018-02-28 12:32:25 -0500

    Commit for Wayne Gretzky

---
+++ src/wayne/gretzky/WayneGretzky.java
@@ -0,0 +1,24 @@
+/*
+WayneGretzky.java
+Create a program that stores the string "Wayne Gretzky" in a string memory
+location and stores 99 in a numeric memory location. The program should 
+display: Wayne Gretzky is number 99 from the memory locations.
+Created by: Lucas N-H
+Created on: February 26, 2018
+Last edited on: February 26, 2018
+*/
+package wayne.gretzky;
+
+public class WayneGretzky {
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String WG;
+        WG = "Wayne Gretzky";
+        int num;
+        num = 99;
+        System.out.println(WG+" is number "+num);
+    }
+}
